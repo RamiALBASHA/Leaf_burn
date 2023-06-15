ca84039f1a9f15a25c1a993b1bfd20be4fc2c485
rami albasha <rami.albasha@inrae.fr>
2023-06-15 18:34:55 +0200

    add infos to config.py

--- sims/config.py
+++ sims/config.py
@@ -1,6 +1,16 @@
 class Pot:
     def __init__(self):
-        self.height = 23
-        self.radius_upper = 13 / 2.
-        self.radius_lower = 10 / 2.
+        self.height = 23  # cm
+        self.radius_upper = 13 / 2.  # cm
+        self.radius_lower = 10 / 2.  # cm
         self.color = (0, 0, 0)
+
+
+class Config:
+    def __init__(self):
+        self.unit_digit: str = 'cm'
+        self.spacing_between_rows: float = 100  # cm
+        self.spacing_on_row: float = Pot().radius_upper * 2.  # cm
+        self.row_angle_with_sout: float = 140
+        self.leaf_height_attribute: str = 'TopPosition'
+        self.extinction_coefficient_wind: float = 0.5
